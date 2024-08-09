35a4aeeca9fd53f0f89a58a18249eda0bb79835c
Adam Scott <ascott.ca@gmail.com>
2024-08-09 08:19:20 -0400

    Fix service worker redirect

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const matchCb = ({url, request, event}) => {
 	if (url.origin !== "https://adamscott.github.io") {
 		return false;
 	}
-	return !url.pathname.startsWith("/godot-website");
+	return url.pathnmame.startsWith("/") && !url.pathname.startsWith("/godot-website");
 };
 
 /** @type {(options: { url: URL, request: Request, event: Event, params: string[] }) => Promise<Response>} */
