9baa6955c0f75c9aa698295169d4c8dd80ca4ccd
Adam Scott <ascott.ca@gmail.com>
2025-02-28 11:24:00 -0500

    Update header background image

--- assets/js/releases/4.4.mjs
+++ assets/js/releases/4.4.mjs
@@ -5,48 +5,25 @@ import detectPlatform from "../modules/detect-browser.mjs";
 
 gsap.registerPlugin(ScrollTrigger);
 
-// Release numbers.
-const RELEASE_NUMBERS_INITIAL_DELAY_S = 1;
-const RELEASE_NUMBERS_DURATION_S = 1;
-const RELEASE_NUMBERS_EASE_NAME = "power2.out";
-const RELEASE_NUMBERS_MAX_BAR_WIDTH_PX = 200;
-
-const releaseNumbersEase = gsap.parseEase(RELEASE_NUMBERS_EASE_NAME);
-const numberFormat = new Intl.NumberFormat("en-US");
-
-for (const el of ["commits", "contributors"]) {
-	const timeline = gsap.timeline();
-	const lines = gsap.utils
-		.toArray(`.release-header .header-numbers-${el} .header-numbers-line`)
-		.reverse();
-	for (let i = 0; i < lines.length; i++) {
-		const line = lines[i];
-		const localTimeline = gsap.timeline();
-		localTimeline.to(line.querySelector(".bar"), {
-			delay: i == 0 ? RELEASE_NUMBERS_INITIAL_DELAY_S : 0,
-			duration: RELEASE_NUMBERS_DURATION_S,
-			ease: RELEASE_NUMBERS_EASE_NAME,
-			width: `${(Number(line.dataset.value) / Number(line.dataset.max)) * RELEASE_NUMBERS_MAX_BAR_WIDTH_PX}px`,
-			onUpdate: () => {
-				line.querySelector(".number").innerText = numberFormat.format(
-					Math.round(
-						releaseNumbersEase(localTimeline.progress()) *
-							Number(line.dataset.value),
-					),
-				);
-			},
-			onComplete: () => {
-				line.querySelector(".number").innerText = numberFormat.format(
-					Number(line.dataset.value),
-				);
-			},
-		});
-		timeline.add(localTimeline);
-
-		// Set commits to 0
-		line.querySelector(".number").innerText = "0";
-	}
-}
+// Parallax scrolling.
+const releaseHeaderBackground = document.querySelector(
+	".release-header-background",
+);
+gsap.to(releaseHeaderBackground, {
+	scrollTrigger: {
+		trigger: ".release-header",
+		start: "top 0%+=64px",
+		end: "bottom 0%",
+		onUpdate: (self) => {
+			const progress = self.progress;
+			releaseHeaderBackground.style.transform = `
+				translateY(${progress * releaseHeaderBackground.getBoundingClientRect().height}px)
+				translateZ(-1px)
+				scale(2)
+			`;
+		},
+	},
+});
 
 // Add a scrolling effect to each card and title.
 const windowHeight = window.innerHeight;
