0c90d067ba3948f2119d7c998f5f5ea72b7e36a1
Godot Organization <>
2025-09-11 23:45:47 +0000

    Deploying to published from @ adamscott/godot-website@4dd98334264840ad064d8cd964d3a15ecc04fc94 🚀

--- misc/scripts/process_video.sh
+++ misc/scripts/process_video.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-GODOT_WEBSITE_FFMPEG="${GODOT_WEBSITE_FFMPEG:-ffmpeg}"
+trap 'exit 1' SIGINT
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "${SCRIPT_DIR%/}/utils.sh"
+
+GODOT_WEBSITE_VIDEO_FFMPEG="${GODOT_WEBSITE_VIDEO_FFMPEG:-ffmpeg}"
 QUALITY="2M"
 INPUT=""
 OUTPUT=""
@@ -10,30 +15,26 @@ show_help() {
 	echo "  -q Quality"
 	echo "  -s Scale 'N:N', with -1 if you want to keep aspect ratio"
 	echo "  -h Help"
+	echo ""
+	echo "Environment variables:"
+	echo "  GODOT_WEBSITE_VIDEO_FFMPEG      Path to \`ffmpeg\` (detault: 'ffmpeg')"
 	returncode="${1:-0}"
 	exit "$returncode"
-}
-
-test_ffmpeg() {
-	if ! command -v "$GODOT_WEBSITE_FFMPEG" >/dev/null 2>&1; then
-		echo "Error: did not find '$GODOT_WEBSITE_FFMPEG', ffmpeg is required" >&2
-		show_help 1
-	fi
 }
 
 parse_args() {
 	while getopts 'q:s:h' opt; do
 		case "$opt" in
-			q)
-				QUALITY="$OPTARG"
-				;;
-			s)
-				SCALE="$OPTARG"
-				;;
-			h)
-				show_help
-				;;
-			?)
+		q)
+			QUALITY="$OPTARG"
+			;;
+		s)
+			SCALE="$OPTARG"
+			;;
+		h)
+			show_help
+			;;
+		?)
 			show_help 1
 			;;
 		esac
@@ -45,16 +46,16 @@ parse_args() {
 
 	test_ffmpeg
 
-	INPUT=${*:$OPTIND:1}
-	OUTPUT=${*:$OPTIND+1:1}
+	INPUT="${*:$OPTIND:1}"
+	OUTPUT="${*:$OPTIND+1:1}"
 
 	if [ -z "$INPUT" ]; then
-		echo "Error: no input given" >&2
+		echo_err "Error: no input given"
 		show_help 1
 	fi
 
 	if [ -z "$OUTPUT" ]; then
-		echo "Error: no output given" >&2
+		echo_err "Error: no output given"
 		show_help 1
 	fi
 }
@@ -69,7 +70,7 @@ main() {
 
 	set -ex
 
-	pass_one_args=(
+	local pass_one_args=(
 		"-i" "$INPUT"
 		"-c:v" "libvpx-vp9"
 		"-b:v" "$QUALITY"
@@ -90,8 +91,8 @@ main() {
 		"$OUTPUT"
 	)
 
-	"$GODOT_WEBSITE_FFMPEG" "${pass_one_args[@]}"
-	"$GODOT_WEBSITE_FFMPEG" "${pass_two_args[@]}"
+	"$GODOT_WEBSITE_VIDEO_FFMPEG" "${pass_one_args[@]}"
+	"$GODOT_WEBSITE_VIDEO_FFMPEG" "${pass_two_args[@]}"
 
 	set +ex
 }
